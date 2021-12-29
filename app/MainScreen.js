670f975b47ccd334d6c9a9fd7cc4332d2766e6bf
Aensdottir <krzakadam74@gmail.com>
2021-12-29 20:52:11 +0100

    Latency working, design basics

--- app/MainScreen.js
+++ app/MainScreen.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useState, Component } from "react";
+import React, { useState, Component, useEffect } from "react";
 import TcpSocket from "react-native-tcp-socket";
 import LottieView from "lottie-react-native";
 
@@ -13,6 +13,8 @@ import {
   Text,
   Flex,
   ScrollView,
+  Pressable,
+  Tabs,
 } from "native-base";
 import TopComponent from "./TopComponent";
 
@@ -23,6 +25,12 @@ const Box = (props) => {
 function App() {
   const { toggleColorMode } = useColorMode();
   const [latencyText, setLatencyText] = useState("Latency");
+  const [connectionText, setConnectionText] = useState("Disconnected");
+
+  /* //Executed on load
+  useEffect(() => {
+    TcpConnect();
+  }, []);*/
 
   return (
     <NativeBaseProvider>
@@ -35,48 +43,67 @@ function App() {
             top={105}
             fontSize="2xl"
           >
-            Latency
+            {connectionText}
           </Text>
           <ScrollView>
-            <Box mx="auto" my="5" px="20" py="5" onPress={toggleColorMode}>
-              <Text color="cyan" fontWeight="bold">
-                {" "}
-                NativeBase
-              </Text>
-            </Box>
+            <Button m={5} onPress={() => TcpConnect("connect")}>
+              Connect
+            </Button>
             <Button size={"md"} m={5} onPress={toggleColorMode}>
               Toggle
             </Button>
-            <Button onPress={() => TcpConnect()}>Click</Button>
-            <Box px="20" py="5" my={2} mx={5} onPress={toggleColorMode}></Box>
-            <Box px="20" py="5" my={2} mx={5} onPress={toggleColorMode}></Box>
-            <Box px="20" py="5" my={2} mx={5} onPress={toggleColorMode}></Box>
-            <Box px="20" py="5" my={2} mx={5} onPress={toggleColorMode}></Box>
-            <Box px="20" py="5" my={2} mx={5} onPress={toggleColorMode}></Box>
+            <ScrollView
+              horizontal={true}
+              bg={"#e8f3f3"}
+              showsHorizontalScrollIndicator={false}
+              h={185}
+            >
+              <Box bg={"#fff"} w={230} flex={1} my={2} ml={5}></Box>
+              <Box bg={"#fff"} w={200} flex={1} my={2} ml={5}></Box>
+              <Flex>
+                <Box bg={"#fff"} flex={1} w={200} my={2} mx={5}></Box>
+                <Box bg={"#fff"} flex={1} w={200} my={2} mx={5}></Box>
+              </Flex>
+            </ScrollView>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
+            <Box px="20" py="5" my={2} mx={5}></Box>
           </ScrollView>
         </ScrollView>
       </Flex>
     </NativeBaseProvider>
   );
 
-  function TcpConnect() {
+  function TcpConnect(command) {
+    setConnectionText("Connecting");
     const options = {
       port: 10144,
       //host: "192.168.1.30",
       host: "78.98.61.153",
     };
     const client = TcpSocket.createConnection(options, () => {
-      client.write("Client");
+      client.write(command);
     });
     client.on("data", function (data) {
-      console.log("message was received", bin2String(data));
-      var latency = Latency(bin2String(data));
-      setLatencyText(latency + "ms");
       client.destroy();
+      console.log("message was received", bin2String(data));
+      const latencyOutput = Latency(bin2String(data));
+      setConnectionText("Connected");
+      setLatencyText(latencyOutput + "ms");
     });
 
     client.on("error", function (error) {
       console.log(error);
+      setConnectionText("Disconnected");
     });
 
     client.on("close", function () {
@@ -84,22 +111,23 @@ function App() {
       client.destroy();
     });
   }
+  function Sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
   function bin2String(array) {
     return String.fromCharCode.apply(String, array);
   }
   function Latency(serverTime) {
     const date = new Date();
-    const dateString = date.toISOString().substring(17, 23); // "2020-01-06T19:57:12.14
-    const latency = (dateString - serverTime.substring(17))
-      .toPrecision(3)
-      .substring(2);
+    const dateString = date.toISOString().substring(14, 23); // "2020-01-06T19:57:12.14
+    const minutes = dateString.substring(0, 2) * 60000;
+    const secondsMs = dateString.substring(3).replace(".", "") * 1;
+    const clientTime = minutes + secondsMs;
+    const latency = (clientTime - serverTime).toString().substring(1);
     console.log(
-      "Servertime: " +
-        serverTime.substring(17) +
-        "\n" +
-        "Clienttime: " +
-        dateString
+      "Servertime: " + serverTime + "\n" + "Clienttime: " + clientTime
     );
+    console.log(minutes + " / " + secondsMs);
     console.log(latency);
     return latency;
   }
